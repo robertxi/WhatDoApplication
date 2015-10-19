6a88da9d11d7a68d1dcf6fe3638de21d5071da12
RobertXi <rxi@ucdavis.edu>
2015-10-18 21:06:54 -0700

    created datasource instance and applied it

--- src/main/java/hello/MyDataSourceFactory.java
+++ src/main/java/hello/MyDataSourceFactory.java
@@ -3,27 +3,31 @@ package hello;
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 import javax.sql.DataSource;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
 
 /**
  * Created by RobertXi on 10/6/15.
  */
 public class MyDataSourceFactory {
+
     static String url = "jdbc:mysql://localhost:3306/whatdodb";
     static String dbName = "whatdodb";
     static String driver = "com.mysql.jdbc.Driver";
     static String dbUser = "wdadmin";
     static String password = "wdpwd";
 
-    public static DataSource getMySQLDataSource() {
-        MysqlDataSource mysqlDS=new MysqlDataSource();
-        mysqlDS.setURL(url);
-        mysqlDS.setUser(dbUser);
-        mysqlDS.setPassword(password);
-        mysqlDS.setDatabaseName(dbName);
+    public static final MyDataSourceFactory INSTANCE = new MyDataSourceFactory();
 
-        return mysqlDS;
+    private MysqlDataSource ds = null;
+
+    private MyDataSourceFactory() {
+        ds=new MysqlDataSource();
+        ds.setURL(url);
+        ds.setUser(dbUser);
+        ds.setPassword(password);
+        ds.setDatabaseName(dbName);
+    }
+
+    public DataSource getDataSource() {
+        return this.ds;
     }
 }
